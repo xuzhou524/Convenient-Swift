4db5537d4c9b33baf109b2c0a26d3d5193a32974
xuzhou524 <xuuohz@163.com>
2016-03-01 17:34:07 +0800

    first commit

---
+++ Convenient-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Convenient-Swift
+//
+//  Created by gozap on 16/3/1.
+//  Copyright © 2016年 xuzhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
