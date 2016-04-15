543f230a3fc288597d30f3740b82b144d568ce2e
xuzhou524 <xuuohz@163.com>
2016-04-15 13:50:24 +0800

    联系人

--- Controller/MoreTableViewController.swift
+++ Controller/MoreTableViewController.swift
@@ -86,6 +86,14 @@ class MoreTableViewController: UITableViewController {
         return baseCell
     }
     
+   override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.row == 3{
+            UIApplication.sharedApplication().openURL(NSURL(string:"tel://18514235240")!);
+        }
+        if indexPath.row == 5{
+            UIApplication.sharedApplication().openURL(NSURL(string:"mqq://im/chat?chat_type=wpa&uin=1043037904&version=1&src_type=web")!);
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
